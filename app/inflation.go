27fdc77123097b050bb82f4c9db66b0c7a3d3005
ChrisCho-H <c.hyunhum@gmail.com>
2024-12-05 15:39:38 +0900

    feat: add custom inflatoin calculator but with default logic

---
+++ app/inflation.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+)
+
+// InflationCalculationFn defines the function required to calculate inflation rate during
+// BeginBlock. It receives the minter and params stored in the keeper, along with the current
+// bondedRatio and returns the newly calculated inflation rate.
+// It can be used to specify a custom inflation calculation logic, instead of relying on the
+// default logic provided by the sdk.
+func CustomInflationCalculationFn(ctx sdk.Context, minter minttypes.Minter, params minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
+	// The target annual inflation rate is recalculated for each previsions cycle. The
+	// inflation is also subject to a rate change (positive or negative) depending on
+	// the distance from the desired ratio (67%). The maximum rate change possible is
+	// defined to be 13% per year, however the annual inflation is capped as between
+	// 7% and 20%.
+
+	// (1 - bondedRatio/GoalBonded) * InflationRateChange
+	inflationRateChangePerYear := math.LegacyOneDec().
+		Sub(bondedRatio.Quo(params.GoalBonded)).
+		Mul(params.InflationRateChange)
+	inflationRateChange := inflationRateChangePerYear.Quo(math.LegacyNewDec(int64(params.BlocksPerYear)))
+
+	// adjust the new annual inflation for this next cycle
+	inflation := minter.Inflation.Add(inflationRateChange) // note inflationRateChange may be negative
+	if inflation.GT(params.InflationMax) {
+		inflation = params.InflationMax
+	}
+	if inflation.LT(params.InflationMin) {
+		inflation = params.InflationMin
+	}
+
+	return inflation
+}
