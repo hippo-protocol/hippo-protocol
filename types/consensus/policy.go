d0caa542ffb1a4be7df093c72036f92ac97b129d
ChrisCho-H <c.hyunhum@gmail.com>
2024-12-17 11:34:52 +0900

    feat: improve tolerability by allowing upto 7 malicious validators

--- types/consensus/policy.go
+++ types/consensus/policy.go
@@ -7,7 +7,7 @@ const (
 	BlockTimeSec    = 6
 	UnbondingPeriod = 60 * 60 * 24 * 7 * 3 * time.Second
 	// staking
-	MaxValidators     = 21
+	MaxValidators     = 22
 	MinCommissionRate = 10
 	// mint
 	Minter              = 25
