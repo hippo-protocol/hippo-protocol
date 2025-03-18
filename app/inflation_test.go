ef1772a62bf12481ad11de8944916ccd8b639296
kek <0114kek@snu.ac.kr>
2025-03-18 21:18:10 +0900

    test: add testcase for inflation

--- app/inflation_test.go
+++ app/inflation_test.go
@@ -15,7 +15,7 @@ import (
 
 func CalcCustomInflation(sdkCtx types.Context) math.LegacyDec {
 	// In CustomInflationCalculationFn, we do not use the minter, so we can pass any value
-	minter := minttypes.Minter{Inflation: math.LegacyNewDec(10), AnnualProvisions: math.LegacyNewDec(10)}
+	minter := minttypes.Minter{Inflation: math.LegacyNewDec(25).Quo(math.LegacyNewDec(100)), AnnualProvisions: math.LegacyNewDec(1)}
 	params := minttypes.Params{
 		BlocksPerYear: consensus.BlocksPerYear,
 		InflationMin:  math.LegacyNewDec(consensus.InflationMin).Quo(math.LegacyNewDec(100)),
@@ -29,6 +29,7 @@ func CalcCustomInflation(sdkCtx types.Context) math.LegacyDec {
 }
 
 func TestInflation(t *testing.T) {
+	// Tolerance is 0.01% for errors resulting from floating point arithmetic
 	tolerance, _ := math.LegacyNewDecFromStr("0.0001")
 
 	// Test Inflation rate at the beginning of the chain
@@ -41,13 +42,43 @@ func TestInflation(t *testing.T) {
 	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear / 2)}, false, log.NewTestLogger(t)))
 	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 6 months")
 
-	// Test Inflation rate after 2 year
-	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 2).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2)) // ~= 8.3%
+	// Test Inflation rate after 1 year
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken)) // ~= 20%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 1 year")
+
+	// Test Inflation rate after 2 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 2).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2)) // ~= 8.33%
 	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 2)}, false, log.NewTestLogger(t)))
 	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 2 years")
 
+	// Test Inflation rate after 3 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 2).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + FirstYearInflatedToken/2)) // ~= 7.69%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 3)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 3 years")
+
+	// Test Inflation rate after 4 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 4).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2)) // ~= 3.57%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 4)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 4 years")
+
 	// Test Inflation rate after 5 years
-	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 4).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2 + (FirstYearInflatedToken / 4))) // ~= 3.4%
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 4).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2 + (FirstYearInflatedToken / 4))) // ~= 3.45%
 	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 5)}, false, log.NewTestLogger(t)))
 	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 5 years")
+
+	// Test Inflation rate after 6 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 8).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2 + (FirstYearInflatedToken/4)*2)) // ~= 1.67%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 6)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 6 years")
+
+	// Test Inflation rate after 7 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 8).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2 + (FirstYearInflatedToken/4)*2 + (FirstYearInflatedToken / 8))) // ~= 1.64%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 7)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 7 years")
+
+	// Test Inflation rate after 8 years
+	expectedInflationRate = math.LegacyNewDec(FirstYearInflatedToken / 16).Quo(math.LegacyNewDec(GenesisSupply + FirstYearInflatedToken*2 + (FirstYearInflatedToken/2)*2 + (FirstYearInflatedToken/4)*2 + (FirstYearInflatedToken/8)*2)) // ~= 0.81%
+	inflation = CalcCustomInflation(types.NewContext(nil, cmtproto.Header{Height: int64(consensus.BlocksPerYear * 8)}, false, log.NewTestLogger(t)))
+	assert.True(t, inflation.GT(expectedInflationRate.Sub(tolerance)) && inflation.LT(expectedInflationRate.Add(tolerance)), "inflation rate should be calculated correctly after 8 years")
 }
