b935b7e6daffad91c2c58e901fcf4a7417733798
ChrisCho-H <c.hyunhum@gmail.com>
2025-09-17 11:48:17 +0900

    feat: use secp256k1-zkp instead of secp256k1

--- sdk/core/src/lib.rs
+++ sdk/core/src/lib.rs
@@ -7,10 +7,10 @@ use aes_gcm::{
     aead::{Aead, AeadCore, OsRng as AesRng},
     Aes256Gcm, Key, KeyInit, Nonce,
 };
-use secp256k1::hashes::{hex::FromHex, sha256, Hash};
-use secp256k1::rand::rngs::OsRng as Secp256k1Rng;
-use secp256k1::{ecdh, Message, PublicKey, Secp256k1, SecretKey};
-use secp256k1::{ecdsa::Signature, hashes::hex::DisplayHex};
+use secp256k1_zkp::ecdsa::Signature;
+use secp256k1_zkp::hashes::{sha256, Hash};
+use secp256k1_zkp::rand::rngs::OsRng as Secp256k1Rng;
+use secp256k1_zkp::{ecdh, Message, PublicKey, Secp256k1, SecretKey};
 use wasm_bindgen::prelude::*;
 
 use types::{Did, EncryptedData, KeyPair};
@@ -41,7 +41,7 @@ pub fn encrypt(data: String, pubkey: String) -> EncryptedData {
     // Alice: one-off key pair to caculate shared secret.
     let (secret_key, public_key) = secp.generate_keypair(&mut Secp256k1Rng);
     // Bob: param pubkey is the one who's able to decrypt the data.
-    let encrypt_to_pubkey = secp256k1::PublicKey::from_str(&pubkey).unwrap();
+    let encrypt_to_pubkey = secp256k1_zkp::PublicKey::from_str(&pubkey).unwrap();
     // Only Alice and Bob can know the secret, which is used as a key to encrypt data.
     let shared_secret = ecdh::SharedSecret::new(&encrypt_to_pubkey, &secret_key).secret_bytes();
 
@@ -54,16 +54,16 @@ pub fn encrypt(data: String, pubkey: String) -> EncryptedData {
     EncryptedData::new(
         public_key.to_string(),
         pubkey,
-        ciphertext.to_lower_hex_string(),
-        nonce.to_lower_hex_string(),
+        hex::encode(&ciphertext),
+        hex::encode(&nonce),
     )
 }
 #[wasm_bindgen]
 pub fn decrypt(data: EncryptedData, privkey: String) -> String {
     // Alice: one-off pubkey to calculate shared secret.
-    let encrypt_from_pubkey = secp256k1::PublicKey::from_str(&data.pubkey_from()).unwrap();
+    let encrypt_from_pubkey = secp256k1_zkp::PublicKey::from_str(&data.pubkey_from()).unwrap();
     // Bob: the one who's able to decrypt the data with privkey.
-    let privkey_to_decrypt = secp256k1::SecretKey::from_str(&privkey).unwrap();
+    let privkey_to_decrypt = secp256k1_zkp::SecretKey::from_str(&privkey).unwrap();
     // Only Alice and Bob can know the secret, which is used as a key to encrypt data.
     let shared_secret =
         ecdh::SharedSecret::new(&encrypt_from_pubkey, &privkey_to_decrypt).secret_bytes();
@@ -72,10 +72,10 @@ pub fn decrypt(data: EncryptedData, privkey: String) -> String {
     let aes_key = Key::<Aes256Gcm>::from_slice(&shared_secret);
     let cipher = Aes256Gcm::new(&aes_key);
     // Nonce hex to bytes
-    let nonce_bytes = Vec::from_hex(&data.nonce()).unwrap();
+    let nonce_bytes = hex::decode(&data.nonce()).unwrap();
     let nonce = Nonce::from_slice(&nonce_bytes);
     // Data hex to bytes
-    let data_bytes = Vec::from_hex(&data.data()).unwrap();
+    let data_bytes = hex::decode(&data.data()).unwrap();
     let decrypted_data = cipher.decrypt(&nonce, data_bytes.as_slice()).unwrap();
 
     String::from_utf8(decrypted_data).unwrap()
@@ -110,9 +110,9 @@ pub fn sha256(data: String) -> String {
 #[wasm_bindgen]
 pub fn ecdh(privkey: String, pubkey: String) -> String {
     // Alice: param privkey is the one who's able to decrypt the data.
-    let alice = secp256k1::SecretKey::from_str(&privkey).unwrap();
+    let alice = secp256k1_zkp::SecretKey::from_str(&privkey).unwrap();
     // Bob: param pubkey is the one who's able to decrypt the data.
-    let bob = secp256k1::PublicKey::from_str(&pubkey).unwrap();
+    let bob = secp256k1_zkp::PublicKey::from_str(&pubkey).unwrap();
     // Only Alice and Bob can know the secret, which is used as a key to sign or encrypt data(or any other).
     let shared_secret = ecdh::SharedSecret::new(&bob, &alice);
     shared_secret.display_secret().to_string()
